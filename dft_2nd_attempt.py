f1a166491b56cb8cd01b4bb07b3249a10f84363e
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2020-11-18 07:02:05 +0000

    Add files via upload

--- dft_2nd_attempt.py
+++ dft_2nd_attempt.py
@@ -596,7 +596,7 @@ else:
     limit2 = math.floor((plotPerErrors[1] + plotRecErrors[1])*10)/10 - 0.1
     limit3 = limit2 + 0.3
     limit4 = math.floor((plotPerErrors[0] + plotRecErrors[0])*10)/10 - 0.2
-    limit5 = limit2 + 0.3
+    limit5 = limit4 + 0.3
     # Do the same as the above but to suit the distribution when s = 25.
 
     fig = plt.figure()
